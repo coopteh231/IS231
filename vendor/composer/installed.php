c352bd188bb850b9143df4294426cf8303f34f53
Владислав Милевский <coopteh231@mail.ru>
2026-03-02 14:45:03 +0700

    Begin test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'coopteh/soa-project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e4cfe1fb596ad0461d11c026092f1d51e3d476e1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'coopteh/soa-project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e4cfe1fb596ad0461d11c026092f1d51e3d476e1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
