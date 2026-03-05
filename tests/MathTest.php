827fabedc50c04ba2c3197f9ed886648ed67788f
coopteh231 <coopteh231@mail.ru>
2026-03-05 10:49:09 +0700

    Bug (#1)
    Bug #1

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -6,7 +6,7 @@ class MathTest extends TestCase {
     /** @test */
     public function it_throws_exception_when_dividing_by_zero() {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage("Делить на ноль нельзя! Дважды НЕЗЯ-Я-Я!!");
+        $this->expectExceptionMessage("Делить на ноль нельзя! Дважды НЕЗЯ-Я-Я!!!!!!!");
 
         Math::divide(10, 0); // Должно выбросить исключение
     }
